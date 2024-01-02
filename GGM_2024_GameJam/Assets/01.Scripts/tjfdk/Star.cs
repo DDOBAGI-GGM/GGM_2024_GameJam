562a900871ba25b652445614713e29f5998d8226
CAFUCH <tjfdk6201@gmail.com>
2024-01-03 02:28:21 +0900

    Ray 수정
    유니티 콜라이더 충돌에서
    Ray로 수정중
    (별, 적) 완료
    무게 개발 중

--- GGM_2024_GameJam/Assets/01.Scripts/tjfdk/Star.cs
+++ GGM_2024_GameJam/Assets/01.Scripts/tjfdk/Star.cs
@@ -1,27 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Star : MonoBehaviour
 {
     [SerializeField] private float distance;
+    [SerializeField] private LayerMask layer;
+    private bool isCollision = false;
 
     private void Update()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, distance);
+        //Collider[] colliders = Physics.OverlapSphere(transform.position, distance);
 
-        foreach (Collider collider in colliders)
+        //foreach (Collider collider in colliders)
+        //{
+        //    if (collider.CompareTag("test1"))
+        //    {
+        //        //if (StageManager.Instance.StarCnt < StageManager.Instance.DustCnt)
+        //        //{
+        //            StageManager.Instance.GetDust(gameObject);
+        //            StageManager.Instance.GetStar(gameObject);
+        //        this.gameObject.SetActive(false);
+        //        //Destroy(this.gameObject);
+        //        //StageManager.Instance.GetStar();
+        //        //}
+        //    }
+        //}
+
+        Ray();
+    }
+    private void Ray()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance, layer);
+
+        if (colliders.Length > 0)
         {
-            if (collider.CompareTag("test1"))
+            if (!isCollision)
             {
-                //if (StageManager.Instance.StarCnt < StageManager.Instance.DustCnt)
-                //{
+                isCollision = true;
+                Debug.Log("µé¾î¿È");
+
+                foreach (Collider collider in colliders)
+                {
                     StageManager.Instance.GetDust(gameObject);
                     StageManager.Instance.GetStar(gameObject);
-                this.gameObject.SetActive(false);
-                //Destroy(this.gameObject);
-                //StageManager.Instance.GetStar();
-                //}
+                    this.gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if (isCollision)
+            {
+                isCollision = false;
+                Debug.Log("³ª°¨");
             }
         }
     }
@@ -29,6 +62,6 @@ public class Star : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, distance);
+        Gizmos.DrawWireSphere(transform.position, distance);
     }
 }
