fa58a782eb80c5331fa93c109ce55110f56e71d8
Hyeok-06 <jun44227@gmail.com>
2024-01-03 23:40:40 +0900

    2D -> 3D 키 변경

--- GGM_2024_GameJam/Assets/01.Scripts/Manager/GameManager.cs
+++ GGM_2024_GameJam/Assets/01.Scripts/Manager/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : Singleton<GameManager>
     {
         timeSinceLastSwitch += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.K) && CanConvert && timeSinceLastSwitch >= switchCooldown)
+        if (Input.GetKeyDown(KeyCode.Q) && CanConvert && timeSinceLastSwitch >= switchCooldown)
         {
             //Debug.Log("시점변경");
             Is3D = !Is3D;
